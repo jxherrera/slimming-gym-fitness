1a687e60fa7b01195b416afdf682b43b4d83aa7d
jxherrera <josuexavi728@gmail.com>
2026-03-07 10:32:07 -0500

    cambios principales al sitio princial

--- frontend-gimnasio/src/App.jsx
+++ frontend-gimnasio/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="logo">SLIMMING<span> GYM</span></div>
+        <nav>
+          <a href="#nosotros">Sobre Nosotros</a>
+          <a href="#planes">Planes</a>
+          <button className="btn-login">Iniciar Sesión</button>
+        </nav>
+      </header>
+
+      <section className="hero">
+        <h1>Transforma tu cuerpo, <br />eleva tu mente</h1>
+        <p>El mejor equipamiento, entrenadores de élite y un ambiente diseñado para que alcances tu máximo potencial. Tu evolución comienza hoy.</p>
+        <button className="btn-cta">Únete Ahora</button>
+      </section>
+
+      <section className="info-section" id="nosotros">
+
+        <div className="card">
+          <h3>Sobre Nosotros</h3>
+          <p>No somos solo pesas y máquinas. Somos una comunidad dedicada a la disciplina y la superación personal. Nuestras instalaciones cuentan con tecnología de punta.</p>
+        </div>
+
+        <div className="card" id="planes">
+          <h3>Nuestros Planes</h3>
+          <p>Desde acceso básico hasta entrenamiento personalizado. Encuentra el plan <span className="highlight">#D20000</span> perfecto que se adapte a tus objetivos y estilo de vida.</p>
+        </div>
+      </section>
     </>
   )
 }

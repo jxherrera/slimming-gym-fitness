79d322a6e9013ce8e402460096b7acc0bab12e8c
jxherrera <josuexavi728@gmail.com>
2026-05-19 22:22:43 -0500

    depuración de App y integración de DB

--- frontend-gimnasio/src/App.jsx
+++ frontend-gimnasio/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import SobreNosotros from './pages/SobreNosotros';
 import Planes from './pages/Planes';
 import Login from './pages/Login';
-import Admin from './pages/admin';
+import Admin from './pages/Admin';
 import Coach from './pages/Coach';
 import Member from './pages/Member';
 
